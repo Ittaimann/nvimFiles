4214d310808a01f4bfe62279378bb47a4c603ab2
Ittai Mann <ittaiam@gmail.com>
2024-09-27 22:03:46 +0100

    started debug work

--- lua/debugging.lua
+++ lua/debugging.lua
@@ -1,7 +1,6 @@
 -------------------- Debugger ----------------------------
 local dap = require('dap')
-require('dap').set_log_level('DEBUG')
--- require("dapui").setup()
+dap.set_log_level('DEBUG')
 
 dap.adapters.lldb = {
   type = 'executable',
@@ -16,7 +15,7 @@ dap.configurations.cpp = {
     type = 'lldb',
     request = 'launch',
     program = function()
-      return require("proj-config").getExecutable() 
+      return require("proj-config").getExecutable()
     end,
     cwd = '${workspaceFolder}',
     stopOnEntry = false,
@@ -31,26 +30,161 @@ M.running= false
 M.frames= widgets.sidebar(widgets.frames)
 M.scopes= widgets.sidebar(widgets.scopes)
 M.watch = widgets.sidebar(widgets.expression)
+M.breakpointList;
 
-function M.run()
-  if dap.session() == nil then
-    dap.continue({true})
-  else 
-    dap.continue({true})
+local sign = vim.fn.sign_define
+
+sign('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
+sign('DapStopped', {text='➡️', texthl='', linehl='', numhl=''})
+sign('DapBreakpointRejected', {text='😔', texthl='', linehl='', numhl=''})
+
+function M.continueSession()
+  M.running = true
+  dap.continue({true})
+end
+
+function M.endDebugSession()
+  if M.running == true then
+    M.running = false
+    M.terminate()
   end
+end
+
+function M.restartSession()
+  if M.running == true then
+    M.restart({true})
+  end
+end
+
+function M.pauseSession()
+  M.pause();
+end
+
+function M.placeBreakPoint()
+  M.toggle_breakpoint()
+end
+
+function M.listBreakPoints()
+  M.list_breakpoints()
+end
+
+function M.stepIn()
+  M.step_into();
+end
+
+function M.stepOut()
+  M.step_out();
+end
+
+function M.stepOver()
+  M.step_over();
+end
+
+--- wrappers around dap
+function M.continue()
+    dap.continue({true})
 end
 
 function M.terminate()
-  if dap.session() ~= nil then
-    dap.terminate()
-  end
+  dap.terminate()
 end
 
 function M.restart()
-  if dap.session() ~= nil then
-    dap.restart()
-  end
+  dap.restart()
 end
 
+function M.run()
+  dap.run()
+end
+function M.sessions()
+  dap.sessions()
+end
+function M.step_out()
+  dap.step_out()
+end
+function M.stop()
+  dap.stop()
+end
+function M.step_back()
+  dap.step_back()
+end
+function M.reverse_continue()
+  dap.reverse_continue()
+end
+function M.set_log_level()
+  dap.set_log_level()
+end
+function M.pause()
+  dap.pause()
+end
+function M.launch()
+  dap.launch()
+end
+function M.close()
+  dap.close()
+end
+function M.disconnect()
+  dap.disconnect()
+end
+function M.up()
+  dap.up()
+end
+function M.down()
+  dap.down()
+end
+function M.goto_()
+  dap.goto_()
+end
+function M.attach()
+  dap.attach()
+end
+function M.list_breakpoints()
+  dap.list_breakpoints()
+end
+function M.run_last()
+  dap.run_last()
+end
+function M.step_over()
+  dap.step_over()
+end
+function M.focus_frame()
+  dap.focus_frame()
+end
+function M.set_session()
+  dap.set_session()
+end
+function M.set_breakpoint()
+  dap.set_breakpoint()
+end
+function M.run_to_cursor()
+  dap.run_to_cursor()
+end
+function M.restart_frame()
+  dap.restart_frame()
+end
+function M.step_into()
+  dap.step_into()
+end
+function M.toggle_breakpoint()
+  dap.toggle_breakpoint()
+end
+function M.session()
+  dap.session()
+end
+function M.terminate()
+  dap.terminate()
+end
+function M.restart()
+  dap.restart()
+end
+function M.status()
+  dap.status()
+end
+function M.clear_breakpoints()
+  dap.clear_breakpoints()
+end
+function M.set_exception_breakpoints()
+  dap.set_exception_breakpoints()
+end
 
 return M
